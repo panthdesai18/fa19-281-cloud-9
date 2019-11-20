18ab3b2db166b9a6e532eb05a1079456d375a948
Ramya Bandi <ramyareddybandi9@gmail.com>
2019-11-20 02:49:29 -0800

    Indented the output into JSON format

--- Locations/main.go
+++ Locations/main.go
@@ -3,23 +3,27 @@ package main
 import (
 	"fa19-281-cloud-9/Locations/controller"
 	"github.com/gorilla/mux"
+	"github.com/unrolled/render"
 	"log"
 	"net/http"
 )
 
 func main() {
 	r := mux.NewRouter()
+	formatter := render.New(render.Options{
+		IndentJSON: true,
+	})
 	r.HandleFunc("/locations", controller.RegisterLocationHandler).
 		Methods("POST")
-	r.HandleFunc("/locations/{locationId}", controller.GetALocationHandler).
+	r.HandleFunc("/locations/{locationId}", controller.GetALocationHandler(formatter)).
 		Methods("GET")
-	r.HandleFunc("/locations", controller.GetAllLocationHandler).
+	r.HandleFunc("/locations", controller.GetAllLocationHandler(formatter)).
 		Methods("GET")
 	/*r.HandleFunc("/locations/{locationId}", controller.UpdateALocationHandler).
 		Methods("PUT")*/
 	r.HandleFunc("/locations/{locationId}", controller.DeleteALocationHandler).
 		Methods("DELETE")
-	r.HandleFunc("/locations/zipcode/{zipcode}", controller.GetLocationsByZipcodeHandler).
+	r.HandleFunc("/locations/zipcode/{zipcode}", controller.GetLocationsByZipcodeHandler(formatter)).
 		Methods("GET")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
