2d2ce79c1d317bdd635263cac454ee962cd95428
Nivali Reddy <nivalireddy.sripati@sjsu.edu>
2019-11-22 13:40:43 -0800

    Added admin login page and made few styling changes

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,24 +1,20 @@
 import React, {Component} from 'react';
 import {Route} from 'react-router-dom';
-import SignUp from './Login/SignUp'
+import Signup from './Login/Signup'
 import Login from './Login/Login'
+import AdminLogin from './Login/AdminLogin'
 import LandingPage from './LandingPage/LandingPage'
 import Menu from './Menu/Menu';
-import Payments from './Payments/Payments';
-import Order from './Order/Order';
-import Location from './Location/Location';
 
 class Main extends Component {
     render(){
         return(
             <div>
-             <Route path="/signup" component={SignUp}/>
+             <Route path="/signup" component={Signup}/>
              <Route path="/login" component={Login}/>
+             <Route path="/adminlogin" component={AdminLogin}/>
              <Route path="/home" component={LandingPage}/>
              <Route path="/menu" component={Menu}/>
-             <Route path="/payments" component={Payments}/>
-             <Route path="/burgerOrder" component={Order} />
-             <Route path="/location" component={Location}/>
             </div>
         )
 
